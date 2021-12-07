e37b7c13ed011cdabe8e1fdfb0c0ef32302ffe05
Helge Egil Seime Pettersen <tubamann@gmail.com>
2021-12-07 15:26:44 +0100

    Added requirements

--- modules/Tools.py
+++ modules/Tools.py
@@ -2,8 +2,9 @@ from math import *
 import numba as nb
 import numpy as np
 
+
 @nb.njit
 def HPM(x):
     """An accurate but quick approximation of the gaussian cumulative density function. 
-        See Vazquez-Leal et al. Math Prob Eng 124029 (2012)"""   
+        See Vazquez-Leal et al. Math Prob Eng 124029 (2012)"""
     return 1. / (exp(-358 * x / 23 + 111 * atan(37 * x / 294)) + 1)
